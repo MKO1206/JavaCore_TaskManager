6141e69de8a75c9b739846d1a5eaedcd21336ec9
MKO1206 <bibliotekacheb@yandex.ru>
2022-06-03 00:26:09 +0700

    Added

---
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -0,0 +1,41 @@
+package ru.netology.javacore;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TodoServer {
+
+    public TodoServer(int port, Todos todos) {
+        Gson gson = new Gson();
+
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                System.out.println("New connection accepted");
+
+
+                final String json = in.readLine();
+                todos = new Gson().fromJson(json, Todos.class);
+
+                switch (todos.getType()) {
+                    case "ADD": todos.addTask(todos.getTask()); break;
+                    case "REMOVE": todos.removeTask(todos.getTask()); break;
+                }
+                out.println(todos.getAllTasks());
+            } catch (IOException e) {
+                System.out.println("Соединение разорвано");
+            }
+        }
+    }
+
+    public void start() throws IOException {
+        int port = 8989;
+        System.out.println("Starting server at " + port + "...");
+    }
+}
---
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -0,0 +1,55 @@
+package ru.netology.javacore;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TodosTests {
+
+    private Todos todos;
+
+    @BeforeAll
+    public static void startTesting() {
+        System.out.println("Запуск тестов");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Действия перед каждым тестом");
+        String task1 = "Отдых";
+        String task2 = "Пробежка";
+        String task3 = "Работа";
+        Todos.tasks.add(task1);
+        Todos.tasks.add(task2);
+        Todos.tasks.add(task3);
+        todos = new Todos();
+    }
+
+    @AfterEach
+    public void refresh() {
+       Todos.tasks.clear();
+    }
+
+    @Test
+    public void whenRemoveTask(){
+        todos.removeTask("Работа");
+        ArrayList<String> taskList = todos.getListTask();
+        Assertions.assertEquals(taskList.size(), 2);
+    }
+
+    @Test
+    public void whenAddNewTask(){
+        Assertions.assertEquals(todos.getListTask().size(), 3);
+        todos.addTask("Учеба");
+        Assertions.assertEquals(todos.getListTask().size(), 4);
+    }
+
+    @Test
+    public void whenGetAllTasks(){
+        String testTaskLIst = "[Отдых, Пробежка, Работа]";
+        assertTrue(todos.getAllTasks().equals(testTaskLIst));
+    }
+}
